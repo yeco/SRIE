e39e07ba890b27aad04e505f1e27e2259645e002
Francisco Campos <hey@francampos.me>
2020-05-11 11:19:16 -0600

    delete reference bootstrap 4 in layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -10,12 +10,7 @@ const Layout = (props) => (
         name='viewport'
         content='width=device-width, initial-scale=1, shrink-to-fit=no'
       />
-      <link
-        rel='stylesheet'
-        href='https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css'
-        integrity='sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh'
-        crossorigin='anonymous'
-      />
+
       <link
         href='https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,300;0,400;0,700;0,900;1,300&family=Roboto:ital,wght@0,300;0,400;0,700;0,900;1,300&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap'
         rel='stylesheet'
