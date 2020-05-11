fba7f98a4a64daa4d762b5f2736d5cb26db6db5e
Francisco Campos <hey@francampos.me>
2020-05-11 11:41:55 -0600

    problem

--- next.config.js
+++ next.config.js
@@ -1,24 +1,7 @@
-<<<<<<< HEAD
 const withImages = require('next-images')
+const dotenv = require('dotenv').config()
 module.exports = withImages()
 
-// module.exports = {
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       issuer: {
-//         test: /\.(js|ts)x?$/,
-//       },
-//       use: ['@svgr/webpack'],
-//     })
-
-//     return config
-//   },
-// }
-=======
-const dotenv = require("dotenv").config();
-
 module.exports = {
-  env: dotenv.parsed
-};
->>>>>>> a5bc678bba29d64d69351fddcbbceb2316dc144f
+  env: dotenv.parsed,
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,14 @@
-import React from "react";
-import App from "next/app";
-import Layout from "../components/layout/Layout";
-<<<<<<< HEAD
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
-=======
-import { appWithTranslation } from "../i18n";
->>>>>>> a5bc678bba29d64d69351fddcbbceb2316dc144f
+import React from 'react'
+import App from 'next/app'
+import Layout from '../components/layout/Layout'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { appWithTranslation } from '../i18n'
 
-import globalStyles from "../styles/global.js";
+import globalStyles from '../styles/global.js'
 
 class SrieApp extends App {
   render() {
-    const { Component, pageProps } = this.props;
+    const { Component, pageProps } = this.props
 
     return (
       <Layout>
@@ -20,8 +17,8 @@ class SrieApp extends App {
           {globalStyles}
         </style>
       </Layout>
-    );
+    )
   }
 }
 
-export default appWithTranslation(SrieApp);
+export default appWithTranslation(SrieApp)
