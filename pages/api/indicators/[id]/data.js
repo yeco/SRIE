1e4fc95c78f080a984072f5947f85d2e5fabaa0f
Javier Camacho <javier.camacho@edify.cr>
2020-05-10 18:34:04 -0600

    Add API input validation

--- pages/api/indicators/[id]/data.js
+++ pages/api/indicators/[id]/data.js
@@ -1,14 +1,25 @@
 import nextConnect from 'next-connect';
 import IndicatorDataService from '../../../../services/IndicatorData.service';
+import InputValidatorUtils from '../../../../utils/InputValidator.utils';
 
 const handler = nextConnect();
 
+const schema = {
+  id: { type: "number", positive: true, integer: true, convert: true },
+  country: { type: "string", min: 2, max: 2 }
+};
+
+const inputValidator = InputValidatorUtils.getInputValidator(schema);
+
 handler.get(async (req, res) => {
-  const indicator = await IndicatorDataService.findByIndicatorId(
-    req.query.id,
-    req.query.country,
-  );
-  res.status(200).json(indicator);
+  if (InputValidatorUtils.validate(inputValidator, req.query, res)) {
+    const indicator = await IndicatorDataService.findByIndicatorId(
+      req.query.id,
+      req.query.country,
+    );
+
+    res.status(200).json(indicator);
+  }
 });
 
 export default (req, res) => handler.apply(req, res);
